c16705fa3d88b2dc8c7796085f73fb656c6cea74
Adrian Yong <Adrian.Yong@reece.com.au>
2017-10-30 14:13:32 +1100

    Update start script to wait for container to start

--- start_container.sh
+++ start_container.sh
@@ -1,3 +1,13 @@
 # This command runs the container and assigns the name 'cuke-runner'
-docker run --name cuke-runner -v /dev/shm:/dev/shm -v ~/projects/customer-orders-entry-frontend/test/cucumber/:/cuke-test -d -p 4444:4444 -p 5900:5900 happycamper28/selenium-ruby-chrome
+if [ ! "$(docker ps -q -f name=cuke-runner)" ]; then
+    # run container
+    docker run --name cuke-runner -v /dev/shm:/dev/shm -v $PWD/:/cuke-test \
+      -d -p 4444:4444 \
+      -p 5900:5900 \
+      -i happycamper28/selenium-ruby-chrome
 
+    while [ ! "$(docker ps -q -f name=cuke-runner)" ]; do
+      echo "$(date) - waiting for container to start"
+      sleep 2
+    done
+fi
